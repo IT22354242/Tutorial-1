2a984cb3ba63f4caba7f2d71eea7dd2c0f285f90
IT22354242 <>
2023-03-09 07:04:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@tute-1/t1ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  float mark1,mark2,avg;
+
+  printf("Input mark 1:");
+  scanf("%f", &mark1);
+
+  printf("Input mark2:");
+  scanf("%f", &mark2);
+
+  avg=(mark1+mark2)/2.0;
+
+  printf("average mark:%.2f",avg);
+  return 0;
+}
---
+++ t1ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int distance,amount;
+
+  printf("Input distance:");
+  scanf("%d", &distance);
+
+  if(distance<=30){
+
+    
+  }
+
+  
+  return 0;
+}
